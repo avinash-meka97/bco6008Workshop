1f8a9268346f3fc4c3f820e91b21627db39aeaa8
Avinash Meka <avinash.meka.92@gmail.com>
2021-08-18 12:15:37 +0530

    predicted new values

--- workshop5.R
+++ workshop5.R
@@ -1,4 +1,42 @@
+#install.packages('MASS')
+#install.packages('ISLR')
+
+library(ISLR)
+library(MASS)
 library(tidymodels)
 
 set.seed(123)
+
+#1 specify the model: type - mode- engine
+lm_spec<-
+  linear_reg()%>%
+  set_mode("regression")%>%
+  set_engine("lm")
+
+data(Boston)
+
+#2 take model specification from step 1 and apply to data = use.fit()
+#and put formula y~x
+lm_fit<-
+  lm_spec%>%
+  fit(data=Boston, medv~lstat)
+
+lm_fit
+
+lm_fit%>%
+  pluck("fit")%>%
+  summary()
+
+tidy(lm_fit)
+
+#3 use the fitted model from step2 to predict new y in new data
+predict(lm_fit, new_data = Boston)
+
+# examining new predicted values
+final_model<-
+  augment(lm_fit, new_data = Boston)
+
+
+
+
 
